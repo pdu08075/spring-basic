ebe653084cef38902d39decccc6968b9864ad9f6
pdu08075 <pdu08075>
2024-08-20 16:49:58 +0900

    2024.08.20.16

--- src/main/java/com/ddyy/springbasic/controller/BasicController.java
+++ src/main/java/com/ddyy/springbasic/controller/BasicController.java
@@ -1,5 +1,10 @@
 package com.ddyy.springbasic.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 // import org.springframework.stereotype.Controller;
@@ -27,10 +32,49 @@ import org.springframework.web.bind.annotation.RestController;
 public class BasicController {
 
     @RequestMapping(value = "/first", method = RequestMethod.GET)
-    public String firstethod() {
+    public String firstMethod() {
         return "첫번째 REST API 요청 응답입니다.";
     }
 
     // HTTP GET method : 클라이언트가 서버로부터 데이터를 받기 위한 메서드
     // Request Body가 존재하지 않음
+    @GetMapping("")
+    public String getMethod() {
+        return "GET Method";
+    }
+
+    // HTTP POST method : 클라이언트가 서버에 리소스를 작성하기 위한 메서드
+    // Request Body가 존재함
+    @PostMapping("")
+    public String postMethod() {
+        return "POST Method";
+    }
+
+    // HTTP PUT method : 클라이언트가 서버에 리소스를 전체 수정하기 위한 메서드
+    // Request Body가 존재함
+    @PutMapping("")
+    public String putMethod() {
+        return "PUT Method";
+    }
+
+    // HTTP PATCH method : 클라이언트가 서버에 리소스를 일부 수정하기 위한 메서드
+    // Request Body가 존재함
+    @PatchMapping("")
+    public String patchMethod() {
+        return "PATCH Method";
+    }
+
+    // HTTP DELETE method : 클라이언트가 서버에 리소스를 삭제하기 위한 메서드
+    // Request Body가 존재하지 않음
+    @DeleteMapping("")
+    public String deleteMethod() {
+        return "DELETE Method";
+    }
+
+    // Method + URL Pattern이 중복되면 서버 실행 중에 에러가 발생
+    // @GetMapping("/first")
+    // public String getFirst() {
+    //     return "get first";
+    // }
+
 }

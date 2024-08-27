f6fe2c22d03d7b19b8297071c56d720eb74816f2
pdu08075 <pdu08075>
2024-08-27 14:51:37 +0900

    2024.08.27.14

---
+++ src/main/java/com/ddyy/springbasic/controller/SampleController.java
@@ -0,0 +1,12 @@
+package com.ddyy.springbasic.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController     // JSON만 받을 예정
+@RequestMapping("/sample")
+public class SampleController {
+    
+    // @PostMapping
+}
---
+++ src/main/java/com/ddyy/springbasic/dto/PostSample1RequestDto.java
@@ -0,0 +1,22 @@
+package com.ddyy.springbasic.dto;
+
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostSample1RequestDto {
+    @NotBlank
+    @Length(max = 10)
+    private String sampleId;
+    @NotNull
+    private Integer sampleComlumn;
+}
